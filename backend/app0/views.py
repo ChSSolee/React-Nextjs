d6763a22600b1f897ed95d6b9d5a3b1ba810c056
Chan-Sol Lee <coldkid1124@gmail.com>
2025-11-14 18:00:20 +0900

    251114 18:00

--- backend/app0/views.py
+++ backend/app0/views.py
@@ -1,6 +1,12 @@
+from .serializers import BookSerializer
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-@api_view(['GET'])
-def hello_world(request):
-    return Response('Hello, World!')
+@api_view(['POST'])
+def create_book(request):
+    serializer = BookSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
